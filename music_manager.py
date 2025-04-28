c18c2d1aa5637736f64691336c69b29d0756bd11
Tin Man's Git <102759745+tinmansgit@users.noreply.github.com>
2025-04-28 14:32:09 -0500

    Update music_manager.py

--- music_manager.py
+++ music_manager.py
@@ -7,7 +7,7 @@ from music_manager_logger import log_error, log_debug
 
 DB_FILENAME = "/bin/Python/Music_Manager/music_db.json"
 EXTS = ('.mp3', '.ogg', '.oga', '.flac')
-PLAYME_SCRIPT = "/bin/Python/PlayMe/playme.py"
+PLAYME_SCRIPT = "~/bin/Python/PlayMe/playme.py"
 
 def load_db():
     log_debug("Attempting to load database.")
@@ -278,7 +278,7 @@ class MusicDBApp(tk.Tk):
     def __init__(self):
         super().__init__()
         try:
-            icon = tk.PhotoImage(file="/bin/Python/Music_Manager/music_manager_icon.png")
+            icon = tk.PhotoImage(file="~/bin/Python/Music_Manager/music_manager_icon.png")
             self.iconphoto(False, icon)
         except Exception as e:
             log_error(f"Failed to load icon: {e}")
